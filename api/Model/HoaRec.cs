45be0a70c3ff9e83d20d967a5629f277d8126dc6
John J Kauflin <johnkauflin@gmail.com>
2024-11-08 09:02:00 -0500

    Check commits

--- api/Model/HoaRec.cs
+++ api/Model/HoaRec.cs
@@ -9,17 +9,17 @@ namespace GrhaWeb.Function.Model
     {
         //public string id { get; set; }
 
-        public hoa_properties property { get; set; }
-        public List<hoa_owners> ownersList { get; set; }
-        public List<hoa_assessments> assessmentsList { get; set; }
-        public List<hoa_communications> commList { get; set; }
-        public List<hoa_sales> salesList { get; set; }
-        public List<TotalDuesCalcRec> totalDuesCalcList { get; set; }
-        public List<string> emailAddrList { get; set; }
-        public string duesEmailAddr { get; set; }   
+        public hoa_properties? property { get; set; }
+        public List<hoa_owners>? ownersList { get; set; }
+        public List<hoa_assessments>? assessmentsList { get; set; }
+        public List<hoa_communications>? commList { get; set; }
+        public List<hoa_sales>? salesList { get; set; }
+        public List<TotalDuesCalcRec>? totalDuesCalcList { get; set; }
+        public List<string>? emailAddrList { get; set; }
+        public string? duesEmailAddr { get; set; }   
 	    public decimal totalDue { get; set; }       // amount = 1234.56m;
 	    public decimal paymentFee { get; set; }     // amount = 1234.56m;
-        public string paymentInstructions { get; set; }   
+        public string? paymentInstructions { get; set; }   
 
 	    //public $adminLevel;               // No longer needed - now using role-based security
 	    //public $paymentButton;            // No longer used
@@ -33,8 +33,8 @@ namespace GrhaWeb.Function.Model
 
     public class TotalDuesCalcRec
     {
-        public string calcDesc { get; set; }
-        public string calcValue { get; set; }
+        public string? calcDesc { get; set; }
+        public string? calcValue { get; set; }
     }
 
 }
