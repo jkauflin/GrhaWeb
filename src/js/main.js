2f857e3848478dcf94a8fa47a7e04d610331dd8f
John J Kauflin <johnkauflin@gmail.com>
2024-11-12 12:35:46 -0500

    limit paid display

--- src/js/main.js
+++ src/js/main.js
@@ -294,17 +294,20 @@ function formatDuesStatementResults(hoaRec) {
         tbody.appendChild(tr)
 
         let tempDuesAmt = '';
+        let maxPaymentHistoryLines = 6;
         for (let index in hoaRec.assessmentsList) {
             let rec = hoaRec.assessmentsList[index]
-
-            tempDuesAmt = '' + rec.DuesAmt;
-            tr = document.createElement('tr')
-            td = document.createElement("td"); td.textContent = rec.FY ; tr.appendChild(td)
-            td = document.createElement("td"); td.textContent = formatMoney(tempDuesAmt); tr.appendChild(td)
-            td = document.createElement("td"); td.textContent = rec.DateDue.substring(0, 10); tr.appendChild(td)
-            td = document.createElement("td"); td.innerHTML = setCheckbox(rec.Paid); tr.appendChild(td)
-            td = document.createElement("td"); td.textContent = rec.DatePaid.substring(0, 10); tr.appendChild(td)
-            tbody.appendChild(tr)
+            // 2024-11-08 JJK - new logic to limit display of historical PAID (or Non-Collectible)
+            if ((!rec.Paid && !rec.NonCollectible) || index < maxPaymentHistoryLines) {
+                tempDuesAmt = '' + rec.DuesAmt;
+                tr = document.createElement('tr')
+                td = document.createElement("td"); td.textContent = rec.FY ; tr.appendChild(td)
+                td = document.createElement("td"); td.textContent = formatMoney(tempDuesAmt); tr.appendChild(td)
+                td = document.createElement("td"); td.textContent = rec.DateDue.substring(0, 10); tr.appendChild(td)
+                td = document.createElement("td"); td.innerHTML = setCheckbox(rec.Paid); tr.appendChild(td)
+                td = document.createElement("td"); td.textContent = rec.DatePaid.substring(0, 10); tr.appendChild(td)
+                tbody.appendChild(tr)
+            }
         }
     }
 
