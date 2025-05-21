f3777dd7863bf5027f20d1b831c1bb32c4786b0b
John J Kauflin <johnkauflin@gmail.com>
2025-05-20 20:14:43 -0400

    still stuck getting jsPDF working

--- src/hoadb/js/pdfModule.js
+++ src/hoadb/js/pdfModule.js
@@ -16,7 +16,18 @@
  *============================================================================*/
 
 //import { jsPDF } from "./jspdf.es.min.js";
-import { jsPDF } from "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.es.min.js";
+//import { jsPDF } from "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.es.min.js";
+
+
+//import { jsPDF } from "jspdf";
+
+// Default export is a4 paper, portrait, using millimeters for units
+/*
+const doc = new jsPDF();
+
+doc.text("Hello world!", 10, 10);
+doc.save("a4.pdf");
+*/
 
 //=================================================================================================================
     // Private variables for the Module
